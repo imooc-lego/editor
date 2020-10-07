cb9b1d636f095b0497c8757c29c6cbfc61cd9357
Viking Zhang <vikingmute@gmail.com>
2020-10-07 21:55:18 +0800

    add EditWrapper resizer, part 1

--- src/components/EditWrapper.vue
+++ src/components/EditWrapper.vue
@@ -1,17 +1,31 @@
 <template>
 <div class="edit-wrapper" @click="itemClick"
+    ref="editWrapper"
     :draggable="true"
     @dragstart="handleDragStart"
     @dragend="handleDragEnd"
     :class="{active: active}" :style="styleProps"
 >
   <slot></slot>
+  <div class='resizers'>
+    <div class='resizer top-left' @mousedown="startResize($event, 'top-left')"></div>
+    <div class='resizer top-right'  @mousedown="startResize($event, 'top-right')"></div>
+    <div class='resizer bottom-left' @mousedown="startResize($event, 'bottom-left')"></div>
+    <div class='resizer bottom-right' @mousedown="startResize($event, 'bottom-right')"></div>
+  </div>
 </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from 'vue'
+import { defineComponent, nextTick, reactive, ref } from 'vue'
 import useStylePick from '../hooks/useStylePick'
+type ResizeDirection = 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right'
+interface OriginalPositions {
+  left: number;
+  right: number;
+  top: number;
+  bottom: number;
+}
 export default defineComponent({
   name: 'ListWrapper',
   props: {
@@ -33,6 +47,7 @@ export default defineComponent({
       x: 0,
       y: 0
     })
+    const editWrapper = ref<null | HTMLElement>(null)
     // need to pick position absolute out, to go with the inner element
     const styleProps = useStylePick(props.props || {}, ['position', 'top', 'left'])
     const itemClick = () => {
@@ -47,7 +62,6 @@ export default defineComponent({
     const handleDragEnd = (e: DragEvent) => {
       const container = document.getElementById('canvas-area') as HTMLElement
       const currentElement = e.currentTarget as HTMLElement
-      console.log(currentElement)
       const finalXCord = e.clientX - container.offsetLeft - gap.x + 'px'
       const finalYCord = e.clientY - container.offsetTop - gap.y + 'px'
       currentElement.style.opacity = ''
@@ -55,11 +69,83 @@ export default defineComponent({
       currentElement.style.left = finalXCord
       context.emit('update-position', { x: finalXCord, y: finalYCord, id: props.id })
     }
+    const caculateSize = (direction: ResizeDirection, e: MouseEvent, positions: OriginalPositions) => {
+      const { left, right, top, bottom } = positions
+      console.log(left)
+      const { pageX, pageY } = e
+      const container = document.getElementById('canvas-area') as HTMLElement
+      const rightWidth = pageX - left
+      const bottomHeight = pageY - top
+      const leftWidth = right - pageX
+      const topHeight = bottom - pageY
+      const leftOffset = pageX - container.offsetLeft
+      const topOffset = pageY - container.offsetTop
+      switch (direction) {
+        case 'top-left':
+          return {
+            width: leftWidth,
+            height: topHeight,
+            top: topOffset,
+            left: leftOffset
+          }
+        case 'top-right':
+          return {
+            width: rightWidth,
+            height: topHeight,
+            top: topOffset
+          }
+        case 'bottom-left':
+          return {
+            width: leftWidth,
+            height: bottomHeight,
+            left: leftOffset
+          }
+        case 'bottom-right':
+          return {
+            width: rightWidth,
+            height: bottomHeight
+          }
+        default:
+          break
+      }
+    }
+    const startResize = (event: MouseEvent, direction: ResizeDirection) => {
+      const currentElement = editWrapper.value as HTMLElement
+      currentElement.draggable = false
+      const { left, right, top, bottom } = currentElement.getBoundingClientRect()
+      const handleMove = (e: MouseEvent) => {
+        if (currentElement) {
+          const size = caculateSize(direction, e, { left, right, top, bottom })
+          if (size) {
+            if (size.left) {
+              currentElement.style.left = size.left + 'px'
+            }
+            if (size.top) {
+              currentElement.style.top = size.top + 'px'
+            }
+            currentElement.style.width = size.width + 'px'
+            currentElement.style.height = size.height + 'px'
+          }
+        }
+      }
+      const handleMouseUp = () => {
+        console.log(123)
+        document.removeEventListener('mousemove', handleMove)
+        currentElement.draggable = true
+        nextTick(() => {
+          document.removeEventListener('mouseup', this as any)
+        })
+      }
+      document.addEventListener('mousemove', handleMove)
+      document.addEventListener('mouseup', handleMouseUp)
+    }
     return {
       itemClick,
       styleProps,
       handleDragStart,
-      handleDragEnd
+      handleDragEnd,
+      startResize,
+      editWrapper
     }
   }
 })
@@ -76,8 +162,40 @@ export default defineComponent({
 }
 .edit-wrapper.active {
   border: 1px solid #1890ff;
+  user-select: none;
+  z-index: 1500;
 }
 .edit-wrapper .l-text-component, .edit-wrapper .l-image-component, .edit-wrapper .l-shape-component {
   position: static !important;
 }
+.edit-wrapper.active .resizers .resizer {
+  width: 10px;
+  height: 10px;
+  border-radius: 50%; /*magic to turn square into circle*/
+  background: white;
+  border: 3px solid #1890ff;
+  position: absolute;
+  display: block;
+}
+.edit-wrapper .resizers .resizer.top-left {
+  left: -5px;
+  top: -5px;
+  cursor: nwse-resize; /*resizer cursor*/
+}
+.edit-wrapper .resizers .resizer.top-right {
+  right: -5px;
+  top: -5px;
+  cursor: nesw-resize;
+}
+.edit-wrapper .resizers .resizer.bottom-left {
+  left: -5px;
+  bottom: -5px;
+  cursor: nesw-resize;
+}
+.edit-wrapper .resizers .resizer.bottom-right {
+  right: -5px;
+  bottom: -5px;
+  cursor: nwse-resize;
+}
+
 </style>
