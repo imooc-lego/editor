fccd2e25aa4d636fbbfb7c28b6c2d50e97624509
Viking Zhang <vikingmute@gmail.com>
2020-11-18 15:52:30 +0800

    update temp build script

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ const isStaging = !!process.env.VUE_APP_IS_STAGING
 const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  publicPath: (isProduction && !isStaging) ? '/editor' : '/',
+  publicPath: (isProduction && !isStaging) ? 'https://oss.imooc-lego.com/editor' : '/',
   css: {
     loaderOptions: {
       less: {
@@ -22,41 +22,41 @@ module.exports = {
       }
     }
   },
-  configureWebpack: config => {
-    // 开启gzip压缩
-    if (isProduction) {
-      config.plugins.push(new CompressionWebpackPlugin({
-        algorithm: 'gzip',
-        test: /\.js$|\.html$|\.json$|\.css/,
-        threshold: 10240,
-        minRatio: 0.8
-      }))
-      // 开启分离js
-      config.optimization = {
-        runtimeChunk: 'single',
-        splitChunks: {
-          chunks: 'all',
-          maxInitialRequests: Infinity,
-          minSize: 500000,
-          cacheGroups: {
-            vendor: {
-              test: /[\\/]node_modules[\\/]/,
-              name (module) {
-                // get the name. E.g. node_modules/packageName/not/this/part.js
-                // or node_modules/packageName
-                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
-                // npm package names are URL-safe, but some servers don't like @ symbols
-                return `npm.${packageName.replace('@', '')}`
-              }
-            }
-          }
-        }
-      }
-      config.stats = {
-        warnings: false
-      }
-    }
-  },
+  // configureWebpack: config => {
+  //   // 开启gzip压缩
+  //   if (isProduction) {
+  //     config.plugins.push(new CompressionWebpackPlugin({
+  //       algorithm: 'gzip',
+  //       test: /\.js$|\.html$|\.json$|\.css/,
+  //       threshold: 10240,
+  //       minRatio: 0.8
+  //     }))
+  //     // 开启分离js
+  //     config.optimization = {
+  //       runtimeChunk: 'single',
+  //       splitChunks: {
+  //         chunks: 'all',
+  //         maxInitialRequests: Infinity,
+  //         minSize: 500000,
+  //         cacheGroups: {
+  //           vendor: {
+  //             test: /[\\/]node_modules[\\/]/,
+  //             name (module) {
+  //               // get the name. E.g. node_modules/packageName/not/this/part.js
+  //               // or node_modules/packageName
+  //               const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+  //               // npm package names are URL-safe, but some servers don't like @ symbols
+  //               return `npm.${packageName.replace('@', '')}`
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //     config.stats = {
+  //       warnings: false
+  //     }
+  //   }
+  // },
   // 打包时不生成.map文件
   productionSourceMap: false
 }
