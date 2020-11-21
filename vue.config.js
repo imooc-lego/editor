e4ac69f016b3d66ba4511c934ac9926f0034a95b
sam <247765564@qq.com>
2020-11-21 10:58:09 +0800

    merge

--- vue.config.js
+++ vue.config.js
@@ -22,41 +22,41 @@ module.exports = {
       }
     }
   },
-  // configureWebpack: config => {
-  //   // 开启gzip压缩
-  //   if (isProduction) {
-  //     config.plugins.push(new CompressionWebpackPlugin({
-  //       algorithm: 'gzip',
-  //       test: /\.js$|\.html$|\.json$|\.css/,
-  //       threshold: 10240,
-  //       minRatio: 0.8
-  //     }))
-  //     // 开启分离js
-  //     config.optimization = {
-  //       runtimeChunk: 'single',
-  //       splitChunks: {
-  //         chunks: 'all',
-  //         maxInitialRequests: Infinity,
-  //         minSize: 500000,
-  //         cacheGroups: {
-  //           vendor: {
-  //             test: /[\\/]node_modules[\\/]/,
-  //             name (module) {
-  //               // get the name. E.g. node_modules/packageName/not/this/part.js
-  //               // or node_modules/packageName
-  //               const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
-  //               // npm package names are URL-safe, but some servers don't like @ symbols
-  //               return `npm.${packageName.replace('@', '')}`
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //     config.stats = {
-  //       warnings: false
-  //     }
-  //   }
-  // },
+  configureWebpack: config => {
+    // 开启gzip压缩
+    if (isProduction) {
+      config.plugins.push(new CompressionWebpackPlugin({
+        algorithm: 'gzip',
+        test: /\.js$|\.html$|\.json$|\.css/,
+        threshold: 10240,
+        minRatio: 0.8
+      }))
+      // 开启分离js
+      config.optimization = {
+        runtimeChunk: 'single',
+        splitChunks: {
+          chunks: 'all',
+          maxInitialRequests: Infinity,
+          minSize: 500000,
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              name (module) {
+                // get the name. E.g. node_modules/packageName/not/this/part.js
+                // or node_modules/packageName
+                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+                // npm package names are URL-safe, but some servers don't like @ symbols
+                return `npm.${packageName.replace('@', '')}`
+              }
+            }
+          }
+        }
+      }
+      config.stats = {
+        warnings: false
+      }
+    }
+  },
   // 打包时不生成.map文件
   productionSourceMap: false
 }
